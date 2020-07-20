a7916cd3db3d7c49f4f46df5ad57124f4f98969d
Derek Finlinson <derek.finlinson@journeyteam.com>
2020-07-20 08:44:45 -0600

    Rename dot files

--- packages/create-powerapps-project/src/plopfile.ts
+++ packages/create-powerapps-project/src/plopfile.ts
@@ -1,9 +1,12 @@
 import path from 'path';
 import { spawnSync } from 'child_process';
+import { rename } from 'just-plop-helpers';
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default (plop: any): void => {
+  plop.setActionType('rename', rename);
+
   plop.setActionType('signAssembly', (answers) => {
     const keyPath = path.resolve(process.cwd(), `${answers.name}.snk`);
     spawnSync(path.resolve(__dirname, '../', 'bin', 'sn.exe'), ['-q', '-k', keyPath], { stdio: 'inherit' });
@@ -18,6 +21,21 @@ export default (plop: any): void => {
         base: '../plop-templates/webresource',
         destination: process.cwd(),
         force: true
+      },
+      {
+        type: 'rename',
+        src: 'gitignore',
+        dest: '.gitignore'
+      },
+      {
+        type: 'rename',
+        src: 'eslintrc.js',
+        dest: '.eslintrc.js'
+      },
+      {
+        type: 'rename',
+        src: 'eslintignore',
+        dest: '.eslintignore'
       }
     ]
   });
@@ -34,12 +52,17 @@ export default (plop: any): void => {
         templateFiles: [
           '../plop-templates/assembly/*.json.hbs',
           '../plop-templates/assembly/*.js',
-          '../plop-templates/assembly/.gitignore',
+          '../plop-templates/assembly/gitignore',
           '../plop-templates/assembly/Entities/EarlyBoundGenerator.xml'
         ],
         base: '../plop-templates/assembly',
         destination: process.cwd(),
         force: true
+      },
+      {
+        type: 'rename',
+        src: 'gitignore',
+        dest: '.gitignore'
       },
       {
         type: 'signAssembly'
