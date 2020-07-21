bb012e67579151c9112931821ad0d2cef2f503ed
Derek Finlinson <derek.finlinson@journeyteam.com>
2020-07-21 15:02:18 -0600

    Remove rename action

--- packages/create-powerapps-project/src/plopfile.ts
+++ packages/create-powerapps-project/src/plopfile.ts
@@ -1,43 +1,9 @@
 import path from 'path';
-import fs from 'fs';
 import { spawnSync } from 'child_process';
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default (plop: any): void => {
-  plop.setActionType('rename', (_answers, config, plop) => {
-    const { src, dest, force } = config;
-
-    if (!src || !dest) {
-      throw new Error('both the "src" and "dest" configuration are needed for this action');
-    }
-
-    let destFileName = '';
-
-    if (typeof dest === 'string') {
-      destFileName = dest;
-    } else if (typeof dest === 'function') {
-      destFileName = dest(src);
-    } else {
-      throw new Error('"dest" can only be a string or function');
-    }
-
-    const srcFilePath = path.join(plop.getDestBasePath(), src);
-    const destFilePath = path.join(plop.getDestBasePath(), destFileName);
-
-    if (!fs.existsSync(srcFilePath)) {
-      throw new Error(`${srcFilePath} does not exist`);
-    }
-
-    if (fs.existsSync(destFilePath) && !force) {
-      throw new Error(`${destFilePath} already exists!`);
-    }
-
-    fs.renameSync(srcFilePath, destFilePath);
-
-    return `successfully renamed ${srcFilePath} to ${destFilePath}`;
-  });
-
   plop.setActionType('signAssembly', (answers) => {
     const keyPath = path.resolve(process.cwd(), `${answers.name}.snk`);
     spawnSync(path.resolve(__dirname, '../', 'bin', 'sn.exe'), ['-q', '-k', keyPath], { stdio: 'inherit' });
@@ -52,21 +18,6 @@ export default (plop: any): void => {
         base: '../plop-templates/webresource',
         destination: process.cwd(),
         force: true
-      },
-      {
-        type: 'rename',
-        src: 'gitignore',
-        dest: '.gitignore'
-      },
-      {
-        type: 'rename',
-        src: 'eslintrc.js',
-        dest: '.eslintrc.js'
-      },
-      {
-        type: 'rename',
-        src: 'eslintignore',
-        dest: '.eslintignore'
       }
     ]
   });
@@ -83,17 +34,12 @@ export default (plop: any): void => {
         templateFiles: [
           '../plop-templates/assembly/*.json.hbs',
           '../plop-templates/assembly/*.js',
-          '../plop-templates/assembly/gitignore',
+          '../plop-templates/assembly/.gitignore',
           '../plop-templates/assembly/Entities/EarlyBoundGenerator.xml'
         ],
         base: '../plop-templates/assembly',
         destination: process.cwd(),
         force: true
-      },
-      {
-        type: 'rename',
-        src: 'gitignore',
-        dest: '.gitignore'
       },
       {
         type: 'signAssembly'
