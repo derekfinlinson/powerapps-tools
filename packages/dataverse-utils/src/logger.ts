64a4be41eca3d938ebe563778ee81c09bd09a9fe
Derek Finlinson <derek.finlinson@journeyteam.com>
2022-01-16 15:38:41 -0700

    Fix logging

--- packages/dataverse-utils/src/logger.ts
+++ packages/dataverse-utils/src/logger.ts
@@ -8,7 +8,7 @@ const log = (method: 'info' | 'warn' | 'error', symbol: string, ...args: any[]) 
   const now = new Date();
   const timestamp = kleur.gray(`[${now.toLocaleTimeString()}]`);
 
-  console[method](timestamp, symbol, args);
+  console[method](timestamp, symbol, ...args);
 };
 
 export const logger = {
