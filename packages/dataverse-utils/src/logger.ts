54c48ba8eec2e8e5d13db586f590c36fd28470e9
Derek Finlinson <derek.finlinson@journeyteam.com>
2022-01-16 16:20:56 -0700

    Switch symbols

--- packages/dataverse-utils/src/logger.ts
+++ packages/dataverse-utils/src/logger.ts
@@ -1,26 +1,21 @@
-import kleur, { } from 'kleur';
+import kleur from 'kleur';
 
-const square = '\u25a0';
-const triangle = '\u25b2';
-const emptySquare = '\u25a1';
+const tick = '√', cross = '×';
 
 const log = (method: 'info' | 'warn' | 'error', symbol: string, ...args: any[]) => {
-  const now = new Date();
-  const timestamp = kleur.gray(`[${now.toLocaleTimeString()}]`);
-
-  console[method](timestamp, symbol, ...args);
+  console[method](symbol, ...args);
 };
 
 export const logger = {
   info(...args: any[]): void {
-    log('info', kleur.green(square), ...args);
+    log('info', kleur.green(tick), ...args);
   },
 
   warn(...args: any[]): void {
-    log('warn', kleur.yellow(triangle), ...args);
+    log('warn', kleur.yellow(cross), ...args);
   },
 
   error(...args: any[]): void {
-    log('error', kleur.red(emptySquare), ...args);
+    log('error', kleur.red(cross), ...args);
   }
 };
