0eeadb4e254859977448b51d33a7c41e6a6ae9a2
Derek Finlinson <derek.finlinson@journeyteam.com>
2022-08-03 15:34:56 -0600

    Add version argument

--- packages/create-powerapps-project/src/index.ts
+++ packages/create-powerapps-project/src/index.ts
@@ -1,14 +1,24 @@
 #!/usr/bin/env node
 import path from 'node:path';
 import { Plop, run } from 'plop';
+import minimist from 'minimist';
 
-Plop.launch({
-  cwd: process.cwd(),
-  configPath: path.join(__dirname, 'plopfile.js')
-}, env => {
-  const options = {
-    ...env,
-    dest: process.cwd() // this will make the destination path to be based on the cwd when calling the wrapper
-  };
-  return run(options, undefined, true)
-});
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const packageJson = require('../package.json');
+
+const argv = minimist(process.argv.slice(2));
+
+if (argv.version || argv.v) {
+  console.log(`${packageJson.version}`);
+} else {
+  Plop.launch({
+    cwd: process.cwd(),
+    configPath: path.join(__dirname, 'plopfile.js')
+  }, env => {
+    const options = {
+      ...env,
+      dest: process.cwd()
+    };
+    return run(options, undefined, true)
+  });
+}
