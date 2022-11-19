2eabeabdd8a7aad6363b20fe449cb0f7e2143c27
Derek Finlinson <derek.finlinson@journeyteam.com>
2022-11-19 01:32:44 -0700

    Add context template

--- packages/powerapps-project-pcf/plopfile.js
+++ packages/powerapps-project-pcf/plopfile.js
@@ -1,51 +1,90 @@
+const inquirer = require('inquirer');
+const inquirerFileTreeSelection = require('inquirer-file-tree-selection-prompt');
+
+inquirer.registerPrompt('file-tree-selection', inquirerFileTreeSelection);
+
 module.exports = function (plop) {
-    plop.setDefaultInclude({ generators: true });
+  plop.setDefaultInclude({ generators: true });
 
-    plop.setGenerator('component', {
-        description: '⚛ react component',
-        prompts: [
-            {
-                type: 'input',
-                name: 'path',
-                message: 'control folder'
-            },
-            {
-                type: 'input',
-                name: 'name',
-                message: 'component name'
-            }
-        ],
-        actions: [
-            {
-                type: 'add',
-                templateFile: 'plop-templates/component.tsx.hbs',
-                path: '{{path}}/components/{{pascalCase name}}.tsx',
-                skipIfExists: true
-            }
-        ]
-    });
+  plop.setGenerator('component', {
+    description: '⚛ react component',
+    prompts: [
+      {
+        type: 'file-tree-selection',
+        name: 'path',
+        onlyShowDir: true,
+        message: 'directory'
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'component name'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        templateFile: 'plop-templates/component.tsx.hbs',
+        path: '{{path}}/{{pascalCase name}}.tsx',
+        skipIfExists: true
+      }
+    ]
+  });
 
-    plop.setGenerator('hook', {
-        description: '⚛ react hook',
-        prompts: [
-            {
-                type: 'input',
-                name: 'path',
-                message: 'control folder'
-            },
-            {
-                type: 'input',
-                name: 'name',
-                message: 'hook name'
-            }
-        ],
-        actions: [
-            {
-                type: 'add',
-                templateFile: 'plop-templates/hook.ts.hbs',
-                path: '{{path}}/hooks/{{camelCase name}}.tsx',
-                skipIfExists: true
-            }
-        ]
-    });
+  plop.setGenerator('hook', {
+    description: '⚛ react hook',
+    prompts: [
+      {
+        type: 'file-tree-selection',
+        name: 'path',
+        onlyShowDir: true,
+        message: 'directory'
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'hook name',
+        validate: (answer) => {
+          if (answer.length > 3 && answer.substring(0, 3) !== 'use') {
+            return "hook name must start with 'use'";
+          }
+
+          return true;
+        }
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        templateFile: 'plop-templates/hook.ts.hbs',
+        path: '{{path}}/{{camelCase name}}.tsx',
+        skipIfExists: true
+      }
+    ]
+  });
+
+  plop.setGenerator('context', {
+    description: '⚛ react context',
+    prompts: [
+      {
+        type: 'file-tree-selection',
+        name: 'path',
+        onlyShowDir: true,
+        message: 'control folder'
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'context name'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        templateFile: 'plop-templates/context.ts.hbs',
+        path: '{{path}}/{{camelCase name}}.tsx',
+        skipIfExists: true
+      }
+    ]
+  });
 };
