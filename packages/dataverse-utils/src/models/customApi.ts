5612295d0c608ed436bc7fc6b48e7b2e98fed275
Derek Finlinson <derek.finlinson@journeyteam.com>
2023-02-15 13:43:31 -0700

    Fix select on custom api create

--- packages/dataverse-utils/src/models/customApi.ts
+++ packages/dataverse-utils/src/models/customApi.ts
@@ -89,7 +89,7 @@ async function createApi(api: CustomApi, apiConfig: WebApiConfig, solution?: str
     options.customHeaders = { 'MSCRM.SolutionUniqueName': solution };
   }
 
-  const result: any = await createWithReturnData(apiConfig, 'customapis', api, '$select=plugintypeid', options);
+  const result: any = await createWithReturnData(apiConfig, 'customapis', api, '$select=customapiid', options);
 
   if (result.error) {
     throw new Error(result.error.message);
