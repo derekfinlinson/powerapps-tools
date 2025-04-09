dede72325fd979ab2566463cab82d4fb41aec207
Derek Finlinson <dcfinlinson@gmail.com>
2025-04-09 11:48:02 -0600

    Import package instead of require

--- packages/dataverse-utils/src/index.ts
+++ packages/dataverse-utils/src/index.ts
@@ -4,8 +4,7 @@ import { program } from 'commander';
 import deploy from './deploy.js';
 import generate from './generate.js';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const packageJson = require('../package.json');
+import packageJson from '../package.json' with { type: 'json' };
 
 program
   .version(packageJson.version)
